579abc699ecc73d7d8749b45e5c98fd92c3c7fb0
karolsw3 <karol.sw3@gmail.com>
2018-06-30 15:22:04 +0200

    Add a footer component

---
+++ web_app/src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+.Footer
+  .Footer__logo
+  p An open-source app by <b>Galactim</b>
+  
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped lang="stylus">
+.Footer
+  position absolute
+  width 100%
+  display flex
+  flex-direction column
+  padding 5px 0
+  align-items center
+  justify-content center
+  text-align center
+  color #cccccc
+  bottom 0 !important
+  &__logo
+    width 32px
+    height 30px
+    background-image url('../../../images/galactim_logo_mini.png')
+    background-size cover
+</style>
