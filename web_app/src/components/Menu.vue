e943621f19b0ef621f5887813708d0047175cbc2
karolsw3 <karol.sw3@gmail.com>
2018-06-30 14:52:02 +0200

    Add a dropdown to the "New game" button

--- web_app/src/components/Menu.vue
+++ web_app/src/components/Menu.vue
@@ -2,7 +2,11 @@
 .Menu
   .Menu__slot
     .Menu__logo
-    button.Menu__button New game
+    .Menu__button.Menu__button--dropdownBase New game
+      .Menu__dropdown
+        button.Menu__button.Menu__button--dropdown Easy
+        button.Menu__button.Menu__button--dropdown Medium
+        button.Menu__button.Menu__button--dropdown Hard
     button.Menu__button Leaderboard
   .Menu__slot
     button.Menu__button Login
@@ -59,8 +63,35 @@ export default {
     border none
     cursor pointer
     transition-duration .12s
+    position relative
     &:hover
       opacity 0.8
+    &--dropdownBase
+      border-radius-bottom-left 0
+      border-radius-bottom-right 0
+      &:hover
+        opacity 1
+        .Menu__dropdown
+          display block
+    &--dropdown
+      background #f4f4f4
+      border-radius none
+      color #787878
+      z-index 2
+      &:hover
+        color white
+        background #a4a4a4
+        opacity 0.8
+  &__dropdown
+    position absolute
+    top 100%
+    left 0
+    width 100%
+    background white
+    border-radius-bottom-left 2px
+    border-radius-bottom-right 2px
+    z-index 3
+    display none
   &__user
     background #0445b7
     border-radius 50%
