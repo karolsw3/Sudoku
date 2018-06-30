adab2d444c37d1860a4bd335877c7f06ec20a662
karolsw3 <karol.sw3@gmail.com>
2018-06-30 07:51:26 +0200

    Add a basic component Template

---
+++ web_app/src/components/componentTemplate.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  .componentTemplate
+</template>
+
+<script>
+  export default {
+    name: 'componentTemplate'
+  }
+</script>
+
+<style scoped lang="stylus">
+  .componentTemplate
+    width 100%
+</style>
