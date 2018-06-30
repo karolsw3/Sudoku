9cc077453893987aef9cef99a5e592d0e977fb4d
karolsw3 <karol.sw3@gmail.com>
2018-06-30 15:19:03 +0200

    Add a footer

--- web_app/src/App.vue
+++ web_app/src/App.vue
@@ -5,16 +5,18 @@
       <router-link to="/about">About</router-link>
     </Menu>
     <router-view/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Menu from '@/components/Menu.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    Menu
+    Menu, Footer
   }
 }
 </script>
