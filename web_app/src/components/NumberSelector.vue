b88c04cd31475acae75363825bd7dfa5585de67d
karolsw3 <karol.sw3@gmail.com>
2018-07-04 20:33:21 +0200

    Add a working number selector component

---
+++ web_app/src/components/NumberSelector.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+.NumberSelector
+  .NumberSelector__slot(v-for="n in 9" @click="$emit('numberSelected', n)") {{n}}
+</template>
+
+<script>
+export default {
+  name: 'NumberSelector'
+}
+</script>
+
+<style scoped lang="stylus">
+.NumberSelector
+  position absolute
+  top 150px
+  left 10px
+  width 150px
+  height 150px
+  display grid
+  grid-template 1fr 1fr 1fr / 1fr 1fr 1fr
+  border 1px solid #eee
+  &__slot
+    width 100%
+    height 100%
+    background white
+    color #d9d9d9
+    font-size 25px
+    display flex
+    justify-content center
+    align-items center
+    border 1px solid #eee
+    cursor pointer
+    box-sizing border-box
+    transition-duration .12s
+    &:hover
+      border 2px solid #0445b7
+      color #0445b7
+</style>
--- web_app/src/views/Play.vue
+++ web_app/src/views/Play.vue
@@ -1,15 +1,22 @@
 <template lang="pug">
   .home
+    NumberSelector(@numberSelected="numberSelected")
     Board
 </template>
 
 <script>
 import Board from '@/components/Board.vue'
+import NumberSelector from '@/components/NumberSelector.vue'
 
 export default {
   name: 'home',
   components: {
-    Board
+    Board, NumberSelector
+  },
+  methods: {
+    numberSelected (number) {
+      console.log(number)
+    }
   }
 }
 </script>
