60521e8b5be00bab33e8b662434eb89876d34250
karolsw3 <karol.sw3@gmail.com>
2018-07-20 10:57:15 +0200

    Improve the boards CSS

--- web_app/src/components/Board.vue
+++ web_app/src/components/Board.vue
@@ -186,21 +186,19 @@ export default {
   position relative
   margin 110px auto 0 auto
   box-sizing border-box
-  border-radius 5px
   &__grid
     position relative
     width 100%
     height 100%
     display grid
-    background #f5f5f5
+    background white
     box-sizing border-box
     grid-template 1fr 1fr 1fr / 1fr 1fr 1fr
     &--main
       width 404px
       height 404px
-      background #f5f5f5
-      border-radius 5px
-      border 1px solid #f5f5f5
+      background white
+      border-radius 8px
   &__slot
     display flex
     align-items center
@@ -216,11 +214,12 @@ export default {
     color #123ffc
     cursor pointer
     transition-duration .25s
+    border-radius 5px
     &:hover
       background #e6e8eb
     &--selected
-      border 4px solid
+      border 3px solid
     &--locked
       background #f5f5f5 !important
-      color #222 !important
+      color #333 !important
 </style>
