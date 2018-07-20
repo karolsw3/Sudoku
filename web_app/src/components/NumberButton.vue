b83a3d30d3bc1ab6492cb80843e809401569257c
karolsw3 <karol.sw3@gmail.com>
2018-07-20 08:10:23 +0200

    Materialize number button

--- web_app/src/components/NumberButton.vue
+++ web_app/src/components/NumberButton.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.NumberButton(:class='{"NumberButton--primary" : isPrimary}')
+md-button.NumberButton(:class='{"md-accent" : isPrimary}')
   slot
 </template>
 
@@ -12,26 +12,9 @@ export default {
 
 <style scoped lang="stylus">
 .NumberButton
+  min-width 0
   width 34px
   height 34px
-  border 1px solid #eee
   margin 0 3px
-  color #777
   font-size 16px
-  border-radius 5px
-  display flex
-  justify-content center
-  align-items center
-  cursor pointer
-  box-sizing border-box
-  text-decoration none
-  transition-duration .12s
-  &:hover
-    background #123ffc
-    border-color #123ffc
-    color white
-  &--primary
-    border-color transparent
-    background #ff4455
-    color white
 </style>
